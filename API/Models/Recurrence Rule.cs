ce2fa718f7b1db35549568d8f3268e1a89c393f0
Gerald <131456327+Geradferd@users.noreply.github.com>
2026-03-26 19:19:56 -0500

    Add work in progress comment to RecurrenceRule class
    Added a comment indicating that the class is a work in progress.

--- API/Models/Recurrence Rule.cs
+++ API/Models/Recurrence Rule.cs
@@ -2,20 +2,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Api.Models;
 
-
+/// Work in progress under Gerald Pruitt
 public class RecurrenceRule
 {
     [Required]
     [MaxLength(100)]
-    public string Frequency {get; set;} = string.Empty; // how often something happens
+    public string Frequency {get; set;} = string.Empty; /// how often something happens
 
     [Required]
-    public List<string> daysOfWeek {get; set;} = new List<string>(); // days of the week what something can happen
+    public List<string> daysOfWeek {get; set;} = new List<string>(); /// days of the week what something can happen
 
     [Required]
     [MaxLength(100)]
-    public string startDateAndEndDate {get; set;} = string.Empty; // start and end date of something
+    public string startDateAndEndDate {get; set;} = string.Empty; /// start and end date of something
 
     [Required]
-    public int maxOccurrences {get; set;} // number of times something happens
+    public int maxOccurrences {get; set;} /// number of times something happens
 }
