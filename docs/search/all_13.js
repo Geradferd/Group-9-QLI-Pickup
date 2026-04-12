1f11c5c8b229e14cb3e843339607878fbce5e9bb
Gerald <131456327+Geradferd@users.noreply.github.com>
2026-04-12 16:02:06 -0500

    Add files via upload

---
+++ docs/search/all_13.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['what_20is_20this_20application_0',['What is this application?',['../md__r_e_a_d_m_e.html#autotoc_md4',1,'']]]
+];
---
+++ docs/search/pages_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['compiler_0',['React Compiler',['../dir_922c588100a187620fdc1533bc178f73.html#autotoc_md1',1,'']]],
+  ['configuration_1',['Expanding the ESLint configuration',['../dir_922c588100a187620fdc1533bc178f73.html#autotoc_md2',1,'']]],
+  ['current_20submission_20project_20setup_2',['Current Submission - Project Setup',['../md__r_e_a_d_m_e.html#autotoc_md9',1,'']]]
+];
