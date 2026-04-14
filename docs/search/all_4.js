e72b8f86409008eb0108fae0f6a50f3778278d16
Gerald <131456327+Geradferd@users.noreply.github.com>
2026-04-14 16:21:16 -0500

    Delete docs directory

--- docs/search/all_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['development_20mode_0',['Development Mode',['../md__r_e_a_d_m_e.html#autotoc_md6',1,'']]]
-];
--- docs/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['orderedtimeperiods_0',['orderedTimePeriods',['../class_calender.html#a786c02248b2116ba57b06da4433d9ed5',1,'Calender']]]
-];
